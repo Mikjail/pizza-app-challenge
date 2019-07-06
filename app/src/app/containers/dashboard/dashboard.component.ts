8a7b68772a69ab5de21ceb8013427869902ce678
Mikjail Salazar <mikjailsalazar@gmail.com>
2019-07-07 01:57:14 +0400

    pizza-challenge #comment adding report section

--- app/src/app/containers/dashboard/dashboard.component.ts
+++ app/src/app/containers/dashboard/dashboard.component.ts
@@ -1,17 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { DashboardService } from './dashboard.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
-
+export class DashboardComponent implements OnInit, AfterViewInit {
+  @ViewChild('lineChart', { static: false }) private chartRef;
+  chart: any;
   constructor(private dashboardService: DashboardService) { }
 
   ngOnInit() {
-    this.getOrders();
+    // this.getOrders();
+  }
+  ngAfterViewInit() { 
+
+    const ctx = this.chartRef.nativeElement.getContext('2d');
+    setTimeout(() => {
+      this.chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ['8am', '11am', '1pm', '4pm', '7pm', '10pm'],
+          datasets: [{
+            data: [10, 20, 3, 40, 4, 10],
+            label: 'Orders',
+            borderColor: '#f88930',
+            backgroundColor: 'rgba(254, 252, 247, 0.3)',
+            fill: true
+          }],
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [{
+              display: true
+            }],
+            yAxes: [{
+              display: true
+            }],
+          }
+        }
+    });
+    });
   }
 
   getOrders() {
