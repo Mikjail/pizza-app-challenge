f661d77d693cd3e11106dbba790f149e6e057627
msalazar <msalazar@accelaero.com>
2019-07-07 17:53:41 +0400

    pizza-challenge adding sortBy to reports

--- util/utils.js
+++ util/utils.js
@@ -24,14 +24,14 @@ const calculateTotalOrders = (orderJson) =>{
         const pendings= _.sumBy(orderJson, ({status})=> (status == ORDER_STATUS.PENDING || status== ORDER_STATUS.ACCEPTED));
         const totalSales = _.sumBy(orderJson,({status,total})=> status == ORDER_STATUS.COMPLETED ? parseFloat(total): 0).toFixed(2);
         const grouByTime = _.groupBy(orderJson,'localTime');
-        _.sortBy(grouByTime, ['loalTime']);
-        
+        const aloh = _.sortBy(grouByTime, ['localTime']);
+
         const timeAndOrders ={
             time:[],
             orders:[]
         }
 
-        Object.keys(grouByTime).forEach(key=>{
+        Object.keys(grouByTime).sort().forEach(key=>{
             timeAndOrders.time.push(key);
             timeAndOrders.orders.push(grouByTime[key].length);
         })
